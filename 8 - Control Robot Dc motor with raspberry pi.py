b6faf350bddd793b4d6fb8f7397ed1203436f632
ali225 <aligamal225@gmail.com>
2018-02-03 20:54:54 +0200

    Edit project robot

--- 8 - Control Robot Dc motor with raspberry pi.py
+++ 8 - Control Robot Dc motor with raspberry pi.py
@@ -1,5 +1,6 @@
 #by eng ali gamal 
 import RPi.GPIO as io
+import time 
 
 io.setmode(io.BCM)
 in1_pin1 = 27 
@@ -14,30 +15,30 @@ io.setup(in2_pin2, io.OUT)
 
 def forward(): 
     io.output(in1_pin1, True)
-	io.output(in1_pin2, False)
-	io.output(in2_pin1, True)
-	io.output(in2_pin2, False)
+    io.output(in1_pin2, False) 
+    io.output(in2_pin1, True)
+    io.output(in2_pin2, False)
 	
 def reverse():	
     io.output(in1_pin1, False)
-	io.output(in1_pin2, True)
-	io.output(in2_pin1, False)
-	io.output(in2_pin2, True)
+    io.output(in1_pin2, True)
+    io.output(in2_pin1, False) 
+    io.output(in2_pin2, True)
 	
 def stop() : 
     io.output(in1_pin1, False)
-	io.output(in1_pin2, False)
-	io.output(in2_pin1, False)
-	io.output(in2_pin2, False)
+    io.output(in1_pin2, False)
+    io.output(in2_pin1, False)
+    io.output(in2_pin2, False)
 
 while True:
     cmd = raw_input("Enter f (forward) or r (reverse) or s (stop) :")
     direction = cmd[0]
-    if direction == "f"
+    if direction == "f":
         forward()
-    if direction == "r"
+    if direction == "r":
          reverse()
-    if direction == "s"
+    if direction == "s":
        stop()
 	   
 	
