3a94ff0ef1bf90ad81b4ff40171494ca5d8f8a26
ali225 <aligamal225@gmail.com>
2018-02-03 19:30:10 +0200

    add project control Dc motro robot

---
+++ 8 - Control Robot Dc motor with raspberry pi.py
@@ -0,0 +1,44 @@
+#by eng ali gamal 
+import RPi.GPIO as io
+
+io.setmode(io.BCM)
+in1_pin1 = 27 
+in2_pin1 = 22 
+in1_pin2 = 20 
+in2_pin2 = 21 
+
+io.setup(in1_pin1, io.OUT)
+io.setup(in2_pin1, io.OUT)
+io.setup(in1_pin2, io.OUT)
+io.setup(in2_pin2, io.OUT)
+
+def forward(): 
+    io.output(in1_pin1, True)
+	io.output(in1_pin2, False)
+	io.output(in2_pin1, True)
+	io.output(in2_pin2, False)
+	
+def reverse():	
+    io.output(in1_pin1, False)
+	io.output(in1_pin2, True)
+	io.output(in2_pin1, False)
+	io.output(in2_pin2, True)
+	
+def stop() : 
+    io.output(in1_pin1, False)
+	io.output(in1_pin2, False)
+	io.output(in2_pin1, False)
+	io.output(in2_pin2, False)
+
+while True:
+    cmd = raw_input("Enter f (forward) or r (reverse) or s (stop) :")
+    direction = cmd[0]
+    if direction == "f"
+        forward()
+    if direction == "r"
+         reverse()
+    if direction == "s"
+       stop()
+	   
+	
+		
